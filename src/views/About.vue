9b372541b66aae7769897a2c0dc6c272fc1fe4af
Harry Higgins <harry.higgins@prezzybox.com>
2022-04-08 14:00:53 +0100

    width

--- src/views/About.vue
+++ src/views/About.vue
@@ -30,7 +30,7 @@ global.lightBoxSource = imageSource;
 <div class="mt-12">
     <h1 class="mb-3">About Me!</h1>
     <div class="flex flex-col space-y-12">
-    <div class="space-y-3 md:w-2/3">
+    <div class="space-y-3">
     <p>I'm a {{age}} year-old Software Engineer from the UK 🇬🇧</p>
     <p>I have two young children who are my fundamental reason for being here. Being a father is the best, most important thing I will ever be - even when I don't feel like a particularly good one!</p>
     <p>In 2021 I got married to the love of my life after being together for 10 years and it was one of the happiest days of my life (see above for the other happiest days of my life).</p>
@@ -38,7 +38,7 @@ global.lightBoxSource = imageSource;
     <p>I keep up to date with the latest developments in - admittedly mostly front-end - tech, and love building fun applications as side-projects outside of work.</p>
     </div>
     
-    <div class="space-y-3 w-2/3">
+    <div class="space-y-3">
     <h2>Whimsical Projects</h2>
     <p>I'll often spend a weekend hashing out an idea for an application or website, mostly fun stuff!</p>
     <p>Recently this has included a <a href="https://festive-mahavira-997ef9.netlify.app/" class="link" target="_new">"Wordle" clone</a> without the one-word-per-day limit, to practice and try and like, be better at words and stuff.</p>
